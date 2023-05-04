8db172d76580e26de00e879571078b196151a467
Jim Bartus <jbartus@fastly.com>
2023-05-04 19:48:24 -0400

    tiny shell formatting tweeks

--- besync.sh
+++ besync.sh
@@ -1,9 +1,9 @@
 source .env
 
-FASTLY_SID=`terraform output -raw fastly_sid`
+FASTLY_SID="$(terraform output -raw fastly_sid)"
 
 curl -X PUT https://dashboard.signalsciences.net/api/v0/corps/${SIGSCI_CORP}/sites/${TF_VAR_site_name}/edgeDeployment/${FASTLY_SID}/backends \
--H "Content-Type:application/json" \
--H "x-api-user:${SIGSCI_EMAIL}" \
--H "x-api-token:${SIGSCI_TOKEN}" \
--H "Fastly-Key: ${FASTLY_KEY}"
+-H "content-type:application/json" \
+-H "x-api-user: ${SIGSCI_EMAIL}" \
+-H "x-api-token: ${SIGSCI_TOKEN}" \
+-H "fastly-key: ${FASTLY_KEY}"
