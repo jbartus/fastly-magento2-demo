575a9c5ca9f6dddae6f73500e5d2d678e336fbc7
Jim Bartus <jbartus@fastly.com>
2023-05-05 16:24:57 -0400

    give the ngwaf a minute to deploy

--- modules/sigsci/main.tf
+++ modules/sigsci/main.tf
@@ -6,13 +6,18 @@ resource "sigsci_site" "demo_site" {
 
 resource "sigsci_edge_deployment" "ngwaf_edge_demo" {
   site_short_name = var.site_name
-  depends_on      = [sigsci_site.demo_site]
+  depends_on = [ sigsci_site.demo_site ]
 }
+
+resource "time_sleep" "sleep_60" {
+  create_duration = "60s"
+  depends_on = [ sigsci_edge_deployment.ngwaf_edge_demo ]
+ }
 
 resource "sigsci_edge_deployment_service" "ngwaf_edge_demo_link" {
   site_short_name  = var.site_name
   fastly_sid       = var.fastly_sid
   activate_version = true
   percent_enabled  = 100
-  depends_on       = [sigsci_edge_deployment.ngwaf_edge_demo]
+  depends_on = [ time_sleep.sleep_60 ]
 }
