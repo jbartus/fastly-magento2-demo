52a91e11470d38378e70d98079d023cf050780e5
Jim Bartus <jbartus@fastly.com>
2023-07-10 15:32:29 -0400

    https://github.com/signalsciences/terraform-provider-sigsci/pull/191

--- modules/sigsci/main.tf
+++ modules/sigsci/main.tf
@@ -8,15 +8,9 @@ resource "sigsci_edge_deployment" "ngwaf_edge_demo" {
   site_short_name = sigsci_site.demo_site.short_name
 }
 
-resource "time_sleep" "wait_for_waf" {
-  create_duration = "100s"
-  depends_on      = [sigsci_edge_deployment.ngwaf_edge_demo]
-}
-
 resource "sigsci_edge_deployment_service" "ngwaf_edge_demo_link" {
-  site_short_name  = sigsci_site.demo_site.short_name
+  site_short_name  = sigsci_edge_deployment.ngwaf_edge_demo.site_short_name
   fastly_sid       = var.fastly_sid
   activate_version = true
   percent_enabled  = 100
-  depends_on       = [time_sleep.wait_for_waf]
 }
