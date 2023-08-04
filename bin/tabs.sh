ea99903e830734b9c82a1e77e63a6f0f2885b256
Jim Bartus <jbartus@fastly.com>
2023-08-04 18:06:13 -0400

    save some typing

---
+++ bin/tabs.sh
@@ -0,0 +1,5 @@
+open -na "Google Chrome" --args --new-window \
+"https://${TF_VAR_site_name}.edgecompute.app" \
+"https://${TF_VAR_site_name}.freetls.fastly.net" \
+"https://manage.fastly.com/stats/real-time/services/$(terraform output -raw fastly_sid)/datacenters/all" \
+"https://dashboard.signalsciences.net/corps/jbartus_lab/sites/${TF_VAR_site_name}?dashboardId=000000000000000000000001&from=-1h"
