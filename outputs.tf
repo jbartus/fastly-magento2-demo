2e4d935160d166125632ebbc9cf390948b363c0a
Jim Bartus <jbartus@fastly.com>
2023-08-15 13:55:46 -0400

    make it clearer what to do next

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,15 @@ output "fastly_sid" {
   description = "fastly service ID"
   value       = fastly_service_vcl.demo_service.id
 }
+
+output "tab1" {
+  value = "https://${var.site_name}.edgecompute.app"
+}
+
+output "tab2" {
+  value = "https://${var.site_name}.global.ssl.fastly.net"
+}
+
+output "tab3" {
+  value = "https://manage.fastly.com/observe/dashboard/system/overview/realtime/${fastly_service_vcl.demo_service.id}"
+}
