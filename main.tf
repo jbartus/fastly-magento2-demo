230fe9e63d33a8cf5b4ca3dd2f840979f42ad6d6
Jim Bartus <jbartus@fastly.com>
2023-08-29 15:52:19 -0400

    workaround master-vcl host override

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "fastly_service_vcl" "demo_service" {
     address        = google_compute_instance.demo_origin_instance.network_interface.0.access_config.0.nat_ip
     name           = "${var.site_name}-origin"
     port           = 443
+    override_host  = "${var.site_name}.freetls.fastly.net"
     use_ssl        = "true"
     ssl_check_cert = "false"
     shield         = "pdx-or-us"
