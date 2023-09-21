b94cdef15688d6fbc39a22bf43bc2d135b285f12
jbartus <561971+jbartus@users.noreply.github.com>
2023-09-21 19:05:20 -0400

    public ip and names

--- outputs.tf
+++ outputs.tf
@@ -3,18 +3,23 @@ output "fastly_sid" {
   value       = fastly_service_vcl.demo_service.id
 }
 
-output "tab1" {
+output "edgeapp" {
   value = "https://${var.site_name}.edgecompute.app"
 }
 
-output "tab2" {
+output "vcl-svc" {
   value = "https://${var.site_name}.freetls.fastly.net"
 }
 
-output "tab3" {
+output "manage" {
   value = "https://manage.fastly.com/observe/dashboard/system/overview/realtime/${fastly_service_vcl.demo_service.id}"
 }
 
-output "tab4" {
+output "dash" {
   value = "https://dashboard.signalsciences.net/corps/${var.sigsci_corp}/sites/${var.site_name}?dashboardId=000000000000000000000001&from=-1h"
+}
+
+output "origin_ip" {
+  description = "public IP of the origin VM"
+  value       = google_compute_instance.demo_origin_instance.network_interface.0.access_config.0.nat_ip
 }
