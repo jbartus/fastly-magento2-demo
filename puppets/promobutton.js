7c1fcf97d303553cdaaad89e69c6d3a92ce98adc
jbartus <561971+jbartus@users.noreply.github.com>
2024-03-28 14:53:35 -0400

    replace deprecated timeout function

--- puppets/promobutton.js
+++ puppets/promobutton.js
@@ -5,14 +5,14 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.setViewport({width: 1600, height: 900});
   await page.goto(process.env.SITE_URL);
-  await page.waitForTimeout(2000);
+  await new Promise(resolve => setTimeout(resolve, 2000));
   await page.waitForSelector('.action.more.button')
   await page.click('.action.more.button');
-  await page.waitForTimeout(2000);
+  await new Promise(resolve => setTimeout(resolve, 2000));
   await page.mouse.wheel({deltaY: 1000});
-  await page.waitForTimeout(100);
+  await new Promise(resolve => setTimeout(resolve, 100));
   await page.mouse.wheel({deltaY: 1000});
-  await page.waitForTimeout(100);
+  await new Promise(resolve => setTimeout(resolve, 100));
   //await page.screenshot({ path: 'promobutton.png' });
   await browser.close();
 })();
